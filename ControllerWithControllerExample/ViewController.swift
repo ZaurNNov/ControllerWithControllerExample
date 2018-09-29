5b14dfa3d085d9d3b91f522ebdacf7a82989e4a0
Zaur Giyasov <zaurusnnov@icloud.com>
2018-09-29 10:32:42 +0300

    Initial Commit

---
+++ ControllerWithControllerExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ControllerWithControllerExample
+//
+//  Created by A1 on 29/09/2018.
+//  Copyright © 2018 Home.Me. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
